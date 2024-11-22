6a7366c833ab367caf477a177b6e15d7cebdd36e
delillo-florencia <delillo.florencia@gmail.com>
2024-11-22 21:29:37 +0100

    tidy up, added mlflow logs

--- get_reps/training_loop.py
+++ get_reps/training_loop.py
@@ -1,119 +1,36 @@
+# PyTorch and related imports
 import torch
 import torch.nn as nn
-import numpy as np
-import esm
-import random
-from torch.utils.data import DataLoader
-from data_utils import ProteinDataset, TaxonIdSampler, get_seq_rep, get_logits
-#For knowledge distillation
-from sklearn.metrics.pairwise import cosine_similarity
 import torch.nn.functional as functional
-import pandas as pd
-import multiprocessing
-from token_mask import mask_single
+from torch.utils.data import DataLoader
 from torch.nn.utils.rnn import pad_sequence
+
+# Data utilities and custom functions
+from data_utils import ProteinDataset, TaxonIdSampler, get_seq_rep, get_logits
+from token_mask import mask_single
+
+# Model and Machine learning - metrics
+from sklearn.metrics.pairwise import cosine_similarity
+import mlflow
+import esm
+
+
+# Data processing and utilities
+import numpy as np
+import pandas as pd
+import random
+import multiprocessing
+from tqdm import tqdm
+
+# Warnings
 import warnings
+
+#Imports for loss
+
+from loss_functions import *
 
 warnings.filterwarnings('ignore')
 
-mse_loss = nn.MSELoss()
-
-def pad_to_match(teacher_kernel, student_kernel):
-    """
-    Just a precaution function. It assures that tokens embeddings in both teacher and student
-    representations have the same shape. This will apply zero-padding the kernel with less dimensions.
-    """
-    rows = max(teacher_kernel.shape[0], student_kernel.shape[0])
-    cols = max(teacher_kernel.shape[1], student_kernel.shape[1])
-    new_teacher_kernel = functional.pad(teacher_kernel, (0, cols - teacher_kernel.shape[1], 
-                                                            0, rows - teacher_kernel.shape[0]))
-    new_student_kernel = functional.pad(student_kernel, (0, cols - student_kernel.shape[1], 
-                                                            0, rows - student_kernel.shape[0]))
-    return new_teacher_kernel, new_student_kernel
-
-
-def kernel_similarity_matrix(repr):
-    """
-    Calculates the cosine similarity between each pair of token embeddings on the kernel
-    """
-    if isinstance(repr, list):
-        repr = torch.stack([torch.tensor(k) for k in repr])  # Convert list to tensor
-    
-    # If kernel is a PyTorch tensor, move it to CPU and convert to NumPy
-    # if not isinstance(kernel, torch.Tensor):
-    #     kernel = kernel.cpu().detach().numpy()  # Move to CPU and detach if needed
-    
-    #print(type(repr))  # Debugging print
-    
-    repr = torch.nn.functional.normalize(repr, p=2, dim=1)
-
-    cosine_similarity_matrix = torch.mm(repr, repr.T)
-
-    #print(cosine_similarity_matrix.shape)
-
-    return cosine_similarity_matrix
-
-def kernel_mse_alignment_loss(teacher_kernel, student_kernel):
-    """
-    Calculates the MSE kernel alignment loss between teacher and student
-    """
-    #print("zero")
-    kernel_similarity_matrix(teacher_kernel)
-    #print("zero")
-    teacher_matrix = torch.tensor(kernel_similarity_matrix(teacher_kernel))
-    #print("zero")
-    student_matrix = torch.tensor(kernel_similarity_matrix(student_kernel))
-    #print("zero")
-
-    if teacher_matrix.shape != student_matrix.shape:
-        teacher_matrix, student_matrix = pad_to_match(teacher_matrix, student_matrix)
-
-    return mse_loss(teacher_matrix, student_matrix)
-
-def logits_mse_loss(teacher_logits, student_logits):
-    """
-    Calculates the MSE loss between teacher and student logits
-    """
-    return mse_loss(teacher_logits, student_logits)
-
-
-class DistillationLoss(nn.Module):
-    def __init__(self, weight_rep=1.0, weight_logits=1.0):
-        super(DistillationLoss, self).__init__()
-        self.weight_rep = weight_rep
-        self.weight_logits = weight_logits
-
-    def forward(self, teacher_rep, teacher_logits, student_rep, student_logits):
-
-        alignment_loss = kernel_mse_alignment_loss(teacher_rep, student_rep)
-
-        logits_loss = logits_mse_loss(teacher_logits, student_logits)
-
-        total_loss = self.weight_rep * alignment_loss + self.weight_logits * logits_loss
-
-        return total_loss
-
-
-def get_seq_rep(results, batch_lens, layer = 33):
-    """
-    Get sequence representations from esm_compute
-    """
-    token_representations = results["representations"][layer]
- 
-    # Generate per-sequence representations via averaging
-    sequence_representations = []
-    for i, tokens_len in enumerate(batch_lens):
-        sequence_representations.append(token_representations[i, 1: tokens_len - 1].mean(0))
- 
-    return sequence_representations
- 
-
-def get_logits(results):
-    """
-    Get logits from esm_compute
-    """
-    logits = results["logits"]
-    return logits
 
 def load_teacher_results(result_type:str, batch_number: int, path ='../data/outputs/'):
     if result_type == 'logi':
@@ -122,7 +39,7 @@ def load_teacher_results(result_type:str, batch_number: int, path ='../data/outp
     elif result_type == 'reps':
         path = path + f'teacher_reps/batch_{batch_number+1}_reps.pt'
     else:
-        raise ValueError('La cagaste mano')
+        raise ValueError('Value error: expecting reps or logi string')
     
     result = torch.load(path)
 
@@ -143,17 +60,25 @@ collection = pd.read_csv(CSV_FILE)
 dataset = ProteinDataset(collection, SEQ_MAX_LEN)
 sampler = TaxonIdSampler(dataset, batch_size=BATCH_SIZE, shuffle=True)
 dataloader = DataLoader(dataset, batch_sampler=sampler, collate_fn=lambda x: x, shuffle=False)
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 #PARAM SET
 num_epochs = 1
 learning_rate = 1e-4
 weight_rep = 0.5
 weight_logits = 0.5
+mlflow.set_experiment("Test_experiment")
 
+#Parameters
+with mlflow.start_run():
+
+    mlflow.log_param("learning_rate", learning_rate)
+    mlflow.log_param("batch_size", batch_size)
+    mlflow.log_param("num_epochs",num_epochs)
+    mlflow.log_param("weight_rep",weight_rep)
+    mlflow.log_param("weight_logits",weight_logits)
 
 #TRAINING LOOP
-
-
 
 checkpoints = True
 cp_dir = "checkpoints"
@@ -184,13 +109,23 @@ optimizer = torch.optim.Adam(student_model.parameters(), lr=learning_rate)
 distillation_loss = DistillationLoss(weight_rep, weight_logits)
 
 
-for epoch in range(num_epochs):
 
+for epoch in tqdm(range(num_epochs), desc="Training Progress", unit="epoch"):
+    cumulative_loss=0.0
     for i, batch in enumerate(dataloader):
-
+        
+        
         # extract sequences and names from the batch
         sequences = [item['sequence'] for item in batch]
         names = [item['protein_id'] for item in batch]
+        #..dont think they may sense since we are calculating metrics per epoch
+        taxon_ids=set([item['taxon_id'] for item in batch])
+        seq_lengths=[item['sequence_length'] for item in batch]
+        mlflow.log_param("taxon_ids",taxon_ids)
+        mlflow.log_param("avg_seq_length",sum(seq_lengths)/len(seq_lengths))
+
+        
+
 
         # prepare data for batch conversion
         if names is None:
@@ -249,8 +184,15 @@ for epoch in range(num_epochs):
         loss = distillation_loss(teacher_reps, teacher_logits, student_reps, masked_student_logits)
         loss.backward()
         optimizer.step()
+        cumulative_loss += loss.item()
 
-    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}')
+    # Compute average metrics for the epoch
+    avg_loss = cumulative_loss / len(dataloader)
+
+    # Log metrics to MLflow
+    mlflow.log_metric("avg_loss", avg_loss, step=epoch)
+
+    print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {avg_loss:.4f}, Accuracy: {accuracy:.4f}')
 
     # tensorflow-like checkpoints
     # if checkpoints:
